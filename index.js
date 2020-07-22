6023bdbb0c5d39ce678a47f6f6035fa7b0c22fbe
Betty <betty.k1995@gmail.com>
2020-07-22 15:46:38 +0200

    added cors middleware

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const householdRouter = require("./routers/household");
 const userRouter = require("./routers/user");
 const taskRouter = require("./routers/task");
 const PORT = 4000
+const corsMiddleWare = require("cors");
 
 const app = express();
 
 const bodyParserMiddleWare = express.json();
 app.use(bodyParserMiddleWare);
 
-// app.use(corsMiddleWare());
+app.use(corsMiddleWare());
 
 if (process.env.DELAY) {
   app.use((req, res, next) => {
