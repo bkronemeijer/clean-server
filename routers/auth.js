8643f646f8b22c2b10be3907ad651c54eff07cd7
Betty <betty.k1995@gmail.com>
2020-07-23 14:37:27 +0200

    added files and created login route WITHOUT password hashes

---
+++ routers/auth.js
@@ -0,0 +1,42 @@
+const bcrypt = require("bcrypt");
+const { Router } = require("express");
+const { toJWT } = require("../auth/jwt");
+const User = require("../models/").user;
+const Task = require("../models").task;
+const Household = require("../models").household;
+
+const router = new Router();
+
+router.post('/login', async (req, res, next) => {
+  try {
+    const {email, password} = req.body
+
+    if (!email || !password) {
+      return res.status(400).send("Please provide email and password")
+    }
+
+    const user = await User.findOne({where: {email}})
+
+    // if (!user || !bcrypt.compareSync(password, user.password)) {
+    //   return res.status(400).send({
+    //     message: "User with that email not found or password incorrect"
+    //   });
+    // }
+
+    if (!user || password !== user.password) {
+      return res.status(400).send({
+        message: "User with that email not found or password incorrect"
+      });
+    }
+
+
+    delete user.dataValues["password"]; // don't send back the password hash
+    const token = toJWT({ userId: user.id });
+    return res.status(200).send({ token, ...user.dataValues });
+  } catch (error) {
+    console.log(error)
+    return res.status(400).send("Something went wrong, sorry")
+  }
+})
+
+module.exports = router
