006dae69003728fc5ecd9447900812daa6d7cdd6
Betty <betty.k1995@gmail.com>
2020-07-26 14:23:41 +0200

    created task route to fetch tasks per household

--- routers/task.js
+++ routers/task.js
@@ -2,9 +2,25 @@ const { Router } = require("express");
 const User = require("../models/").user;
 const Task = require("../models").task;
 const Household = require("../models").household;
+const authMiddleware = require("../auth/middleware")
 
 const router = new Router();
 
-router.get('/')
+router.post('/', authMiddleware, async (req, res, next) => {
+  try {
+    const id = parseInt(req.body.id)
+
+    if (!id) {
+      return res.status(400).send("Please provide an id")
+    }
+
+    const task = await Task.findAll({where: {householdId: id}})
+
+    res.json(task)
+  } catch (error) {
+    console.log(error)
+    res.status(400).send("Something went wrong, sorry")
+  }
+})
 
 module.exports = router
