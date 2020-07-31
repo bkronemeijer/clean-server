1d7d9238a9560a558595eaa2f76395b38ba7ceef
Betty <betty.k1995@gmail.com>
2020-07-31 10:09:55 +0200

    added currently assigned to.. to task page"

--- routers/task.js
+++ routers/task.js
@@ -1,25 +1,30 @@
-const { Router } = require("express");
-const User = require("../models/").user;
-const Task = require("../models").task;
-const Household = require("../models").household;
+const { Router } = require("express")
+const Sequelize = require('sequelize')
+const moment = require('moment')
+const Op = Sequelize.Op;
+const User = require("../models/").user
+const Task = require("../models").task
+const TaskSchedule = require("../models").taskSchedule
+const Household = require("../models").household
 const authMiddleware = require("../auth/middleware")
 
 const router = new Router();
 
 router.post('/', authMiddleware, async (req, res, next) => {
   try {
-    const id = parseInt(req.body.id)
+    const householdId = parseInt(req.body.householdId)
+    const recurrence = parseInt(req.body.recurrence)
 
-    if (!id) {
-      return res.status(400).send("Please provide an id")
+    if (!householdId || !recurrence) {
+      return res.status(400).send({message: "Please provide an id"})
     }
 
-    const task = await Task.findAll({where: {householdId: id}})
+    const task = await Task.findAll({where: {householdId}, include: {model: TaskSchedule, where: {deadline: {[Op.between]: [new Date(), moment().add(recurrence, 'd')] }}, include: [User]}})
 
     res.json(task)
   } catch (error) {
     console.log(error)
-    res.status(400).send("Something went wrong, sorry")
+    res.status(400).send({message: "Something went wrong, sorry"})
   }
 })
 
