6babb7f3abcf03698dc18d1485b9b351da8f2284
Betty <betty.k1995@gmail.com>
2020-07-31 11:34:23 +0200

    set up patch user route

--- routers/user.js
+++ routers/user.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const User = require("../models/").user;
 const Task = require("../models").task;
 const Household = require("../models").household;
+const authMiddleware = require("../auth/middleware")
 
 const router = new Router();
 
@@ -13,9 +14,29 @@ router.get('/', async (req, res, next) => {
       return res.status(400).send("Please provide an id")
     }
 
-    const household = await household.findByPk(id)
+    const household = await Household.findByPk(id)
 
     res.json(household)
+  } catch (error) {
+    console.log(error)
+    res.status(400).send("Something went wrong, sorry")
+  }
+})
+
+router.patch('/', async (req, res, next) => {
+  try {
+    const id = parseInt(req.body.id)
+    const name = req.body.name
+
+    if (!id) {
+      return res.status(400).send("Please provide an id")
+    }
+
+    const userToUpdate = await User.findByPk(id)
+
+    const updatedUser = await userToUpdate.update({name})
+
+    res.json(updatedUser)
   } catch (error) {
     console.log(error)
     res.status(400).send("Something went wrong, sorry")
